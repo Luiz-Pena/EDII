4e010e412fd8f34a63a23b8539b761dbbb9b417c
Luiz-Pena <lh1316245@gmail.com>
2025-06-22 17:26:28 -0300

    odeio ficar gripado

---
+++ Rabin_Karp_Matcher/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
+
+void matcher (string completo, string parte, int base, int primo) {
+    int padrao = 0, texto = 0;
+
+    for (int i=0; i<parte.length(); i++){
+
+    }
+
+}
